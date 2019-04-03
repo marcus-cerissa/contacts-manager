006fe0516b333731540b68599085fb1ef66600d3
Cerissa Antill <cerissa.antill@gmail.com>
2019-04-03 15:17:19 -0500

    add getMenu(){

--- src/exercise/ContactsManager.java
+++ src/exercise/ContactsManager.java
@@ -58,6 +58,21 @@ public class ContactsManager {
 
     }
 
+
+    public String getMenu() {
+
+        return "1. View contacts.\n" +
+                "2. Add a new contact.\n" +
+                "3. Search a contact by name.\n" +
+                "4. Delete an existing contact.\n" +
+                "5. Exit.\n" +
+                "Enter an option (1, 2, 3, 4 or 5):";
+
+    }
+
+
+
+
     public static void main(String[] args) {
 
 
